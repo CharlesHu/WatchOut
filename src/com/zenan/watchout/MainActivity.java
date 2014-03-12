4b1eabd5027fb0c725e42b7dcb410aad70fd6a87
Charles Hu <twolittlecats@163.com>
2014-03-12 14:38:16 +0800

    test sensor

--- src/com/zenan/watchout/MainActivity.java
+++ src/com/zenan/watchout/MainActivity.java
@@ -1,20 +1,59 @@
 package com.zenan.watchout;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	private SensorManager mSensorManager = null;
+	private Sensor mSensor = null;
+	private float x, y, z;
+	private TextView text;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		text = (TextView) findViewById(R.id.text);
+		
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	}
 
+	SensorEventListener listener = new SensorEventListener() {
+		
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			x = event.values[SensorManager.DATA_X];
+			y = event.values[SensorManager.DATA_Y];
+			z = event.values[SensorManager.DATA_Z];
+			
+		}
+		
+		@Override
+		public void onAccuracyChanged(Sensor arg0, int arg1) {
+			
+		}
+	};
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			mSensorManager.registerListener(listener, mSensor, SensorManager.SENSOR_DELAY_GAME);
+			
+			text.setText("x:" + x + "y:" + y + "z:" + z);
+		}
+		return super.onTouchEvent(event);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
